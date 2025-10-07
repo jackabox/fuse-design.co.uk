75c3e0723bc995dad1251f0d0a493e2d23e452da
Jack Whiting <hi@jackwhiting.co.uk>
2025-10-07 17:02:27 +0100

    header change

--- src/js/header.js
+++ src/js/header.js
@@ -1,47 +1,47 @@
 export default () => ({
-  showNav: false,
-  atTop: true,
-  scrollingUp: true,
+	showNav: false,
+	atTop: true,
+	scrollingUp: true,
 
-  init() {
-    this.resizeEvent()
-    this.atTop = window.scrollY < 200
-  },
+	init() {
+		this.resizeEvent()
+		this.atTop = window.scrollY < 200
+	},
 
-  loadSearch() {
-    this.closeNavItem()
-  },
+	loadSearch() {
+		this.closeNavItem()
+	},
 
-  closeNavItem() {
-    this.showNav = false
-  },
+	closeNavItem() {
+		this.showNav = false
+	},
 
-  handleScroll() {
-    this.atTop = window.scrollY < 200
-    this.scrollingUp = document.body.getBoundingClientRect().top > this.scrollPos
-    this.scrollPos = document.body.getBoundingClientRect().top
-  },
+	handleScroll() {
+		this.atTop = window.scrollY < 200
+		this.scrollingUp = document.body.getBoundingClientRect().top > this.scrollPos
+		this.scrollPos = document.body.getBoundingClientRect().top
+	},
 
-  toggleNav() {
-    this.showNav = !this.showNav
-    this.overflowClass()
-  },
+	toggleNav() {
+		this.showNav = !this.showNav
+		this.overflowClass()
+	},
 
-  resizeEvent() {
-    this.showNav = window.innerWidth < 1024 ? false : true
-    this.overflowClass()
-  },
+	resizeEvent() {
+		this.showNav = window.innerWidth <= 1024 ? false : true
+		this.overflowClass()
+	},
 
-  overflowClass() {
-    const html = document.querySelector("html")
-    const body = document.querySelector("body")
+	overflowClass() {
+		const html = document.querySelector("html")
+		const body = document.querySelector("body")
 
-    if (this.showNav && window.innerWidth <= 1024) {
-      html.classList.add("overflow-hidden")
-      body.classList.add("overflow-hidden")
-    } else {
-      html.classList.remove("overflow-hidden")
-      body.classList.remove("overflow-hidden")
-    }
-  },
+		if (this.showNav && window.innerWidth <= 1024) {
+			html.classList.add("overflow-hidden")
+			body.classList.add("overflow-hidden")
+		} else {
+			html.classList.remove("overflow-hidden")
+			body.classList.remove("overflow-hidden")
+		}
+	},
 })
