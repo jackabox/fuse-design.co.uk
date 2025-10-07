d812ed2901d52a03f7cd31d90fd6b9c4005e02e3
Jack Whiting <hi@jackwhiting.co.uk>
2025-10-07 17:06:51 +0100

    removing autoheight

--- src/js/testimonials.js
+++ src/js/testimonials.js
@@ -1,29 +1,29 @@
 import EmblaCarousel from "embla-carousel"
 import Fade from "embla-carousel-fade"
 import Autoplay from "embla-carousel-autoplay"
-import AutoHeight from 'embla-carousel-auto-height'
+import AutoHeight from "embla-carousel-auto-height"
 import { addPrevNextBtnsClickHandlers, addDotBtnsAndClickHandlers } from "./embala"
 
 export default () => ({
-  init() {
-    console.log(this.$el)
-    console.log(this.$el.querySelector(".embla__viewport"))
-    const prevBtnNode = this.$el.querySelector(".embla__button--prev")
-    const nextBtnNode = this.$el.querySelector(".embla__button--next")
-    const dotsNode = this.$el.querySelector(".embla__dots")
+	init() {
+		console.log(this.$el)
+		console.log(this.$el.querySelector(".embla__viewport"))
+		const prevBtnNode = this.$el.querySelector(".embla__button--prev")
+		const nextBtnNode = this.$el.querySelector(".embla__button--next")
+		const dotsNode = this.$el.querySelector(".embla__dots")
 
-    const emblaApi = EmblaCarousel(
-      this.$refs.slider,
-      {
-        loop: true,
-      },
-      [Fade(), Autoplay({ playOnInit: true, delay: 10000 }), AutoHeight()],
-    )
+		const emblaApi = EmblaCarousel(
+			this.$refs.slider,
+			{
+				loop: true,
+			},
+			[Fade(), Autoplay({ playOnInit: true, delay: 10000 })],
+		)
 
-    const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(emblaApi, prevBtnNode, nextBtnNode)
-    const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(emblaApi, dotsNode)
+		const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(emblaApi, prevBtnNode, nextBtnNode)
+		const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(emblaApi, dotsNode)
 
-    emblaApi.on("destroy", removePrevNextBtnsClickHandlers)
-    emblaApi.on("destroy", removeDotBtnsAndClickHandlers)
-  },
+		emblaApi.on("destroy", removePrevNextBtnsClickHandlers)
+		emblaApi.on("destroy", removeDotBtnsAndClickHandlers)
+	},
 })
